6acf32ea137e8280b561816c9f0418d90194c77c
Fate <dxfinitelynotfate@gmail.com>
2023-11-12 11:23:58 +0200

    Adding state

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,71 @@
-import './App.css'
-import { useState, useEffect } from 'react'
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const API_KEY: string = "920e3e4ff64c8dc8a9f87cedbcf4fb16";
 
 type FetchError = {
-  evoked: boolean,
-  message?: string
-}
+  evoked: boolean;
+  message?: string;
+};
 
 const App: React.FC = () => {
-  const key: string = '920e3e4ff64c8dc8a9f87cedbcf4fb16'
-  const city: string = "Berlin"
-  const url: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`
+  const [city, setCity] = useState<string>("");
 
+  const url: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
-  const [loading, setLoading] = useState<boolean>(true)
-  const [error, setError] = useState<FetchError>({evoked: false})
-
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<FetchError>({ evoked: false });
 
   useEffect(() => {
     let ignore = false;
-    
+
     const getWeatherData = async () => {
       setLoading(true);
       try {
-        setError({evoked: false});
-        const response = await fetch(url).then(response => response.json());
-    
-        if(response.cod === '404') {
-          throw new Error(`City '${city}' not found! You have to introduce a valid city name.`)
+        setError({ evoked: false });
+
+        if (city == "") throw new Error(`Provide city name`);
+
+        const response = await fetch(url).then((response) => response.json());
+
+        if (response.cod === "404") {
+          throw new Error(`City '${city}' not found!`);
         }
-    
+
         if (!ignore) {
-          console.log(response)
+          console.log(response);
         }
       } catch (err) {
         setError({
           evoked: true,
-          message: (err as Error).message
+          message: (err as Error).message,
         });
       }
-    
+
       setLoading(false);
     };
-    
+
     getWeatherData();
-    
-    return (() => { ignore = true; });
-  }, [url]);
-        
-    
-  if(loading) return <h1>Loading...</h1>
-  if(error.evoked) return <h1>Error: {error.message}</h1>;
-    
+
+    return () => {
+      ignore = true;
+    };
+  }, [url, city]);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <>
-      <h1>Hello world</h1>
-    </>
-  )
-}
+      <input
+        type="text"
+        placeholder="City"
+        onChange={(e) => setCity(e.target.value)}
+        value={city}
+      />
 
-export default App
+      {!error.evoked ? <h1>Hello world</h1> : <h1>Error: {error.message}</h1>}
+    </>
+  );
+};
+
+export default App;
